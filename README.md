c3e47a5f904b9b265759d2982bbd28a754baa062
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2019-06-25 11:43:17 +0300

    Add skeleton app

---
+++ README.md
@@ -0,0 +1,5 @@
+# Digest generator tool
+
+This application allows to add links to chapters (digest articles) and generate output markup by targets using templates.
+
+ToDo: Add to readme how to install, run etc.
---
+++ src/settings.php
@@ -0,0 +1,34 @@
+<?php
+
+use Monolog\Logger;
+
+return [
+    'settings' => [
+        'displayErrorDetails' => true, // set to false in production
+        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+
+        // Renderer settings
+        'renderer' => [
+            'template_path' => __DIR__ . '/../templates/',
+        ],
+
+        // Monolog settings
+        'logger' => [
+            'name' => 'slim-app',
+            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+            'level' => Logger::DEBUG,
+        ],
+
+        // Database
+        'db' => [
+            'driver' => 'sqlite',
+//            'host' => 'localhost',
+            'database' => __DIR__ . '../database/database.sqlite',
+            'username' => '',
+            'password' => '',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ]
+    ],
+];
